523a2f9d5f29fdcf198eff32c30b554538a4bf70
Miguel C <miguelcanepa.pessoal@gmail.com>
2025-02-04 18:31:45 -0400

    Configure TypeORM database connection and add core application modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,36 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { EnvModule } from './commons/env/env.module';
-import { envSchema } from './commons/env';
+import { Env, envSchema } from './commons/env';
+import { AuthModule } from './modules/auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './modules/user/user.module';
+import { CryptoModule } from './commons/crypto/crypto.module';
+import { HealthcheckModule } from './modules/healthcheck/healthcheck.module';
+import { SessionModule } from './modules/session/session.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      validate: (env) => envSchema.parse(env)
+      validate: (env) => envSchema.parse(env),
     }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: new ConfigService<Env, true>().get('DATABASE_URL'),
+      entities: [__dirname + '/**/*.entity{.js,.ts}'],
+      synchronize: true,
+      logging: true,
+      autoLoadEntities: true,
+    }),
+
     EnvModule,
+    AuthModule,
+    UserModule,
+    CryptoModule,
+    HealthcheckModule,
+    SessionModule,
   ],
   controllers: [],
   providers: [],
--- src/main.ts
+++ src/main.ts
@@ -1,53 +1,52 @@
-import { ValidationPipe } from "@nestjs/common";
-import { NestFactory } from "@nestjs/core";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-import { apiReference } from "@scalar/nestjs-api-reference";
-import helmet from "helmet";
-import { AppModule } from "./app.module";
-import { EnvService } from "./commons/env/env.service";
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
+import helmet from 'helmet';
+import { AppModule } from './app.module';
+import { EnvService } from './commons/env/env.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const envService = app.get(EnvService);
 
   app.enableCors({
-    origin: [envService.get("CLIENT_URL")],
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    origin: [envService.get('CLIENT_URL')],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
   const config = new DocumentBuilder()
-    .setTitle("Buscar Advogado")
-    .setVersion("1.0")
-    .setDescription("API para buscar advogados")
+    .setTitle('Buscar Advogado')
+    .setVersion('1.0')
+    .setDescription('API para buscar advogados')
     .addBearerAuth()
-    .addTag("buscar-advogado")
+    .addTag('buscar-advogado')
     .build();
 
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api", app, documentFactory, {
-    jsonDocumentUrl: "swagger/json",
+  SwaggerModule.setup('api', app, documentFactory, {
+    jsonDocumentUrl: 'swagger/json',
   });
 
   app.use(
-    "/reference",
+    '/reference',
     apiReference({
       spec: {
-        url: "/swagger/json",
+        url: '/swagger/json',
       },
-    })
+    }),
   );
 
   app.use(helmet());
-
 
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
       whitelist: true,
-    })
+    }),
   );
 
-  await app.listen(envService.get("PORT"));
+  await app.listen(envService.get('PORT'));
 }
 bootstrap();
