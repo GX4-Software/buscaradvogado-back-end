b6733ca68539e6da9a2582ace60c2d374aa74291
Miguel C <miguelcanepa.pessoal@gmail.com>
2025-02-03 22:53:12 -0400

    Configure Swagger, Helmet, CORS, and global validation for NestJS application

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,53 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { apiReference } from "@scalar/nestjs-api-reference";
+import helmet from "helmet";
+import { AppModule } from "./app.module";
+import { EnvService } from "./commons/env/env.service";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const envService = app.get(EnvService);
+
+  app.enableCors({
+    origin: [envService.get("CLIENT_URL")],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle("Buscar Advogado")
+    .setVersion("1.0")
+    .setDescription("API para buscar advogados")
+    .addBearerAuth()
+    .addTag("buscar-advogado")
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, documentFactory, {
+    jsonDocumentUrl: "swagger/json",
+  });
+
+  app.use(
+    "/reference",
+    apiReference({
+      spec: {
+        url: "/swagger/json",
+      },
+    })
+  );
+
+  app.use(helmet());
+
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    })
+  );
+
+  await app.listen(envService.get("PORT"));
 }
 bootstrap();
